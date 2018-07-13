5e43c48a6ec11e315904ca857bda4e14c87f4974
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-13 12:50:17 -0400

    pulls the stocks data from Yahoo finance and prints it on console

--- ScraperProject/Program.cs
+++ ScraperProject/Program.cs
@@ -13,6 +13,9 @@ namespace ScraperProject
     {
         static void Main(string[] args)
         {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("disable-popup-blocking");
+
             IWebDriver chromeDriver = new ChromeDriver();
             chromeDriver.Navigate().GoToUrl("https://login.yahoo.com");
 
@@ -20,13 +23,21 @@ namespace ScraperProject
             IWebElement username = chromeDriver.FindElement(By.XPath("//*[@id=\"login-username\"]"));
             username.SendKeys("surekha.srinivasan@yahoo.com");
             username.Submit();
-            
-            WebDriverWait wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(8));
+
+            WebDriverWait wait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(10));
             IWebElement password = wait.Until(driver => chromeDriver.FindElement(By.Id("login-passwd")));
             password.SendKeys("Careerdevs");
             password.SendKeys(Keys.Enter);
 
-            chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_0/view/v1");            
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            chromeDriver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_0/view/v1");
+            
+            var closePopUp = chromeDriver.FindElement(By.XPath("//*[@id=\"fin-tradeit\"]/div[2]/div/div/div[2]/button[2]"));
+            closePopUp.Click();
+
+            var stocks = chromeDriver.FindElements(By.XPath("//*[@id=\"main\"]/section/section[2]/div[2]/table/tbody/tr[*]/td[*]"));
+            foreach (var stock in stocks)
+                Console.WriteLine(stock.Text);
         }
     }    
 }
